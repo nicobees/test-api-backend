dc0ec20423cae972672e8778d8a0f0fb2266d557
Nicola Abis <n.abis@e-motion.com>
2022-02-05 23:41:52 +0100

    test: first integration test for express application

---
+++ src/__tests__/app.test.ts
@@ -0,0 +1,28 @@
+import { Application } from 'express'
+import request from 'supertest'
+
+import appExpress from '@root/app'
+import { ServerConfig } from '@root/config'
+import { httpResponseStatus } from '@root/utils'
+
+describe('express app', () => {
+  let app: Application
+  const envConfig: ServerConfig = {
+    environment: 'dev',
+    port: 3000
+  }
+
+  beforeAll(async () => {
+    app = await appExpress(envConfig)
+  })
+
+  it('should return http error code 404 if request is different from POST', async () => {
+    await request(app)
+      .get('/')
+      .expect('Content-Type', /json/)
+      .expect(httpResponseStatus.NOT_FOUND)
+      .expect({ code: 404, msg: 'Not Found' })
+  })
+})
+
+export {}
